5cd18ae102137863e13856b0e51aea39ff9b78ee
palashitb <palash.sharma@students.iiit.ac.in>
2021-09-22 20:22:38 +0530

    after like 3-4 hours of debugging, finally solved dependency issues

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,6 +2,11 @@ import React, {Component} from 'react';
 import axios from 'axios';
 import ResponsiveDrawer from './templates/Sidebar';
 import AppBar from '@material-ui/core/AppBar';
+import Box from '@mui/material/Box';
+import Switch from '@mui/material/Switch';
+import Paper from '@mui/material/Paper';
+import Collapse from '@mui/material/Collapse';
+import FormControlLabel from '@mui/material/FormControlLabel';
 import Button from '@material-ui/core/Button';
 import CameraIcon from '@material-ui/icons/PhotoCamera';
 import Card from '@material-ui/core/Card';
@@ -69,9 +74,29 @@ function Copyright() {
         './static/img/3.jpg',
         './static/img/4.jpg'
     ]
+    const icon = (
+      <Paper sx={{ m: 1 }} elevation={4}>
+        <Box component="svg" sx={{ width: 100, height: 100 }}>
+          <Box
+            component="polygon"
+            sx={{
+              fill: (theme) => theme.palette.common.white,
+              stroke: (theme) => theme.palette.divider,
+              strokeWidth: 1,
+            }}
+            points="0,100 50,00, 100,100"
+          />
+        </Box>
+      </Paper>
+    );
     
     export default function Dashboard() {
+      const [checked, setChecked] = React.useState(false);
 
+      const handleChange = () => {
+        setChecked((prev) => !prev);
+      };
+    
       axios.get("http://localhost:4000/user/legit", {withCredentials: true})
         .then(res => {
             // console.log(res.data, "this is the response");
@@ -98,65 +123,29 @@ function Copyright() {
             </Toolbar> */}
           </AppBar>
           <main>
-            {/* Hero unit */}
-            <div className={classes.heroContent}>
-              <Container maxWidth="sm">
-                <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
-                  Library
-                </Typography>
-                <Typography variant="h5" align="center" color="textSecondary" paragraph>
-                  {/* Something short and leading about the collection below—its contents, the creator, etc.
-                  Make it short and sweet, but not too short so folks don&apos;t simply skip over it
-                  entirely. */}
-                </Typography>
-                {/* <div className={classes.heroButtons}>
-                  <Grid container spacing={2} justify="center">
-                    <Grid item>
-                      <Button variant="contained" color="primary">
-                        Main call to action
-                      </Button>
-                    </Grid>
-                    <Grid item>
-                      <Button variant="outlined" color="primary">
-                        Secondary action
-                      </Button>
-                    </Grid>
-                  </Grid>
-                </div> */}
-              </Container>
-            </div>
-            <Container className={classes.cardGrid} maxWidth="md">
-              {/* End hero unit */}
-              <Grid container spacing={4}>
-                {cards.map((card, ind) => (
-                  <Grid item key={card} xs={12} sm={6} md={4}>
-                    <Card className={classes.card}>
-                      <CardMedia
-                        className={classes.cardMedia}
-                        image="https://source.unsplash.com/random"
-                        title="Image title"
-                      />
-                      <CardContent className={classes.cardContent}>
-                        <Typography gutterBottom variant="h5" component="h2">
-                          Book Details
-                        </Typography>
-                        <Typography>
-                          Description of the book, language etc.
-                        </Typography>
-                      </CardContent>
-                      <CardActions>
-                        <Button size="small" color="primary">
-                          View
-                        </Button>
-                        <Button size="small" color="primary">
-                          Delete
-                        </Button>
-                      </CardActions>
-                    </Card>
-                  </Grid>
-                ))}
-              </Grid>
-            </Container>
+            <Box sx={{ height: 300 }}>
+              <FormControlLabel
+                control={<Switch checked={checked} onChange={handleChange} />}
+                label="Show"
+              />
+              <Box
+                sx={{
+                  '& > :not(style)': {
+                    display: 'flex',
+                    justifyContent: 'space-around',
+                    height: 120,
+                    width: 250,
+                  },
+                }}
+              >
+                <div>
+                <Collapse in={checked}>{icon}</Collapse>
+                <Collapse in={checked} collapsedSize={40}>
+                  {icon}
+                </Collapse>
+              </div>
+            </Box>
+            </Box>
           </main>
           {/* Footer */}
           <footer className={classes.footer}>
