6c6f61b89880bae25add49e084709c4530b2f9b0
Palash Sharma <palashsharma@Palashs-MacBook-Air.local>
2022-12-20 22:16:15 +0530

    trying for deployment

--- src/components/Variables.js
+++ src/components/Variables.js
@@ -1,4 +1,6 @@
 module.exports = {
-    frontendURL: 'http://localhost:3000/',
-    backendURL: 'http://127.0.0.1:8787/',
+    // frontendURL: 'http://localhost:3000/',
+    frontendURL: 'https://spashal-github-io.pages.dev/',
+    // backendURL: 'http://127.0.0.1:8787/',
+    backendURL: "https://blog-site.palash-sharma.workers.dev/"
 };
